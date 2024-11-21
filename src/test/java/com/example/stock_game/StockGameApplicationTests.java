e51d6a4e780e2a432a3f27931f83d0721a399652
Talia Pulsifer <taliapulsifer@outlook.com>
2024-11-21 17:15:09 -0600

    Goal: Interactive mini-game that uses real-time stock prices, showcases automation, and engages users on personal website.

---
+++ src/test/java/com/example/stock_game/StockGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stock_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
